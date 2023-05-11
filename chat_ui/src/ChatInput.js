97c56b93b8e4a933eaa34e6084be23e596c575b6
Tiago Deliberali Santos <tiago.deliberali@gmail.com>
2023-05-11 17:47:17 -0300

    fixed input issue

--- chat_ui/src/ChatInput.js
+++ chat_ui/src/ChatInput.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Form, Input, Button } from "antd";
 
 // A presentational component that renders the input field and the send button for the user
 function ChatInput({ value, onChange, onSubmit, loading }) {
+  // Use a ref to access the form instance
+  const formRef = useRef(null);
+
+  // Use an effect to set the value of the input field to the value prop
+  useEffect(() => {
+    // Check if the form instance exists
+    if (formRef.current) {
+      // Set the value of the input field to the value prop
+      formRef.current.setFieldsValue({ message: value });
+    }
+  }, [value]);
+
   return (
     <Form
       layout="inline"
       style={{ width: "100%", margin: "24px 0" }}
       onFinish={onSubmit}
+      // Assign the ref to the form instance
+      ref={formRef}
     >
       <Form.Item name="message" style={{ flex: 1 }}>
         <Input
